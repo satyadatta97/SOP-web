bc091824af898299b8e08790d97179e3348eceb9
vijaykumar <boravijaykumar595@gmail.com>
2024-12-13 15:17:41 +0530

    added new

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,24 +1,24 @@
 export const SERVER_API_URL = "";
 
-export const TMS_APP_URL = "http://dev.tms.iplexr.com/";
-export const DEMAND_APP_URL = "http://dev.demand.iplexr.com/";
-export const SCM_WEB_APP_URL = "http://dev.iplexr.com/";
-export const CONFIG_ADMIN_APP_URL = "http://142.93.215.84:8083/"
-export const SCHEDULEJOB_APP_URL = "http://dev.schedulejobs.iplexr.com/";
-export const PROMOTION_APP_URL = "http://dev.promotion.iplexr.com/";
-export const SUPPLY_APP_URL = "http://dev.supply.iplexr.com/";
+// export const TMS_APP_URL = "http://dev.tms.iplexr.com/";
+// export const DEMAND_APP_URL = "http://dev.demand.iplexr.com/";
+// export const SCM_WEB_APP_URL = "http://dev.iplexr.com/";
+// export const CONFIG_ADMIN_APP_URL = "http://142.93.215.84:8083/"
+// export const SCHEDULEJOB_APP_URL = "http://dev.schedulejobs.iplexr.com/";
+// export const PROMOTION_APP_URL = "http://dev.promotion.iplexr.com/";
+// export const SUPPLY_APP_URL = "http://dev.supply.iplexr.com/";
 export const POS_APP_URL = "https://pos.iplexr.com/home";
 export const VMS_APP_URL = "https://vms.iplexr.com/login";
 
-// export const TMS_APP_URL = "http://localhost:3001/";
-// export const DEMAND_APP_URL = "http://localhost:3002/";
-// export const SCM_WEB_APP_URL = "http://localhost:4200/";
-//  export const CONFIG_ADMIN_APP_URL = "http://localhost:3006/"
-//  export const SCHEDULEJOB_APP_URL = "http://localhost:3003/";
-//  export const PROMOTION_APP_URL = "http://localhost:3004/";
-//  export const SUPPLY_APP_URL = "http://localhost:3005/";
+export const TMS_APP_URL = "http://localhost:3001/";
+export const DEMAND_APP_URL = "http://localhost:3002/";
+export const SCM_WEB_APP_URL = "http://localhost:4200/";
+ export const CONFIG_ADMIN_APP_URL = "http://localhost:3006/"
+ export const SCHEDULEJOB_APP_URL = "http://localhost:3003/";
+ export const PROMOTION_APP_URL = "http://localhost:3004/";
+ export const SUPPLY_APP_URL = "http://localhost:3005/";
 
-//export const ENVIRONMENT = "LOCAL";
-  export const ENVIRONMENT = "DEV";
+export const ENVIRONMENT = "LOCAL";
+//  export const ENVIRONMENT = "DEV";
 //  export const ENVIRONMENT = "PROD";
 
